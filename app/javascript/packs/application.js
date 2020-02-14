eb57ac9a57c86b9264f0b3ea590cb41365fbb0fa
Edwin Nguyen <edwin.nguyen@edhec.com>
2020-02-14 17:46:44 +0100

    vf ?

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,25 +1,8 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
-
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-
 import 'bootstrap';
 import $ from 'jquery';
 import 'select2';
 
-console.log('Hello World from Webpacker')
+import 'select2/dist/css/select2.css';
 
 const cards = document.querySelectorAll(".card-category")
 
@@ -33,6 +16,7 @@ const hideWhenHover = (card) => {
 };
 
 cards.forEach(hideWhenHover);
+
 
 $(document).ready(function() {
     $('.js-example-basic-single').select2({
