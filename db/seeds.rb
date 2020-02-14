4474e882d8cd7e2bcbbecddd958bc9ed3cea2012
Edwin Nguyen <edwin.nguyen@edhec.com>
2020-02-14 11:16:05 +0100

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,52 +16,77 @@ Dose.destroy_all
 puts "#{Cocktail.count} Cocktails in database"
 puts "#{Ingredient.count} Ingredients in database"
 puts "#{Dose.count} Doses in database"
-# url_ingredients = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-# ingredients_hash = JSON.parse(open(url_ingredients).read)
-# ingredients = []
-# ingredients_hash['drinks'].each do |ingredient|
-#   ingredients << ingredient['strIngredient1']
+
+# url_cocktails = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail"
+# cocktails_hash = JSON.parse(open(url_cocktails).read)
+# cocktails = []
+# cocktails_hash['drinks'].each do |cocktail|
+#   cocktails << { name: cocktail['strDrink'], image_url: cocktail['strDrinkThumb'] }
 # end
-# ingredients.each do |ingredient|
-#   Ingredient.create(name: ingredient)
+# cocktails.each do |cocktail|
+#   cocktail = Cocktail.new(cocktail)
+#   url_details = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=#{cocktail.name.gsub("ó", "o")}"
+#   details_hash = JSON.parse(open(url_details).read)
+#   ingredients =[]
+#   doses = []
+#   i = 1
+#   while !details_hash['drinks'].first["strIngredient#{i}"].nil?
+#     ingredients << details_hash['drinks'].first["strIngredient#{i}"]
+#     doses << details_hash['drinks'].first["strMeasure#{i}"]
+#     i += 1
+#   end
+#   doses.each_with_index do |element, index|
+#     ingredient_name = ingredients[index]
+#     p ingredient_name
+#     ingredient = Ingredient.where(name: ingredient_name).take
+#     ingredient = Ingredient.new(name: ingredients[index]) if ingredient.nil?
+#     p ingredient
+#     dose = Dose.new(description: element)
+
+#     p ingredient
+#     dose.ingredient = ingredient
+#     ingredient.save
+#     dose.cocktail = cocktail
+#     dose.save
+#     cocktail.save
+#   end
 # end
 
-url_cocktails = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail"
-cocktails_hash = JSON.parse(open(url_cocktails).read)
-cocktails = []
-cocktails_hash['drinks'].each do |cocktail|
-  cocktails << {name: cocktail['strDrink'], image_url: cocktail['strDrinkThumb']}
-end
-cocktails.each do |cocktail|
-  cocktail = Cocktail.new(cocktail)
-  url_details = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=#{cocktail.name.gsub("ó", "o")}"
-  details_hash = JSON.parse(open(url_details).read)
-  ingredients =[]
-  doses = []
-  i = 1
-  while !details_hash['drinks'].first["strIngredient#{i}"].nil?
-    ingredients << details_hash['drinks'].first["strIngredient#{i}"]
-    doses << details_hash['drinks'].first["strMeasure#{i}"]
-    i += 1
+###########################
+
+('a'..'z').each do |letter|
+  url_cocktails = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=#{letter}"
+  cocktails_hash = JSON.parse(open(url_cocktails).read)
+  if !cocktails_hash['drinks'].nil?
+    cocktails_hash['drinks'].each_with_index do |cocktail, index|
+      cocktail = Cocktail.new({ name: cocktail['strDrink'], image_url: cocktail['strDrinkThumb'] })
+      ingredients = []
+      doses = []
+      i = 1
+      while !cocktails_hash['drinks'][index]["strIngredient#{i}"].nil?
+        ingredients << cocktails_hash['drinks'][index]["strIngredient#{i}"]
+        doses << cocktails_hash['drinks'][index]["strIngredient#{i}"]
+        i += 1
+      end
+      if !doses.nil?
+        doses.each_with_index do |element, index|
+          ingredient_name = ingredients[index]
+          p ingredient_name
+          ingredient = Ingredient.where(name: ingredient_name).take
+          ingredient = Ingredient.new(name: ingredients[index]) if ingredient.nil?
+          p ingredient
+          dose = Dose.new(description: element)
+
+          p ingredient
+          dose.ingredient = ingredient
+          ingredient.save
+          dose.cocktail = cocktail
+          dose.save
+          cocktail.save
+        end
+      end
+    end
   end
-  doses.each_with_index do |element, index|
-
-    ingredient_name = ingredients[index]
-    p ingredient_name
-    ingredient = Ingredient.where(name: ingredient_name).take
-    ingredient = Ingredient.new(name: ingredients[index]) if (ingredient.nil?)
-    p ingredient
-    dose = Dose.new(description: element)
-
-    p ingredient
-    dose.ingredient = ingredient
-    ingredient.save
-    dose.cocktail = cocktail
-    dose.save
-    cocktail.save
-  end
-
-
 end
 
 
